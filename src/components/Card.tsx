a6b4cfdaf78ae4e0e5c4444fbab4247cec59c173
Erik S. Carlsten <ecarlste@gmail.com>
2025-04-04 12:02:29 -0600

    implement increment/decrement buttons

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,15 +1,18 @@
+import { useState } from "react";
 import Count from "./Count";
 import CountButtons from "./CountButtons";
 import ResetButton from "./ResetButton";
 import { Title } from "./Title";
 
 function Card() {
+  const [count, setCount] = useState(0);
+
   return (
     <div className="card">
       <Title />
-      <Count number={0} />
+      <Count number={count} />
       <ResetButton />
-      <CountButtons />
+      <CountButtons setCount={setCount} />
     </div>
   );
 }
--- src/components/CountButtons.tsx
+++ src/components/CountButtons.tsx
@@ -1,12 +1,30 @@
 import { MinusIcon, PlusIcon } from "@radix-ui/react-icons";
+import { Dispatch, SetStateAction } from "react";
 
-function CountButtons() {
+type CountButtonsProps = {
+  setCount: Dispatch<SetStateAction<number>>;
+};
+
+type CountButtonOperation = "increment" | "decrement";
+
+function CountButtons({ setCount }: CountButtonsProps) {
+  function handleUpdateCount(operation: CountButtonOperation = "increment") {
+    if (operation === "decrement") {
+      setCount((prev) => Math.max(prev - 1, 0));
+    } else {
+      setCount((prev) => prev + 1);
+    }
+  }
+
   return (
     <div className="button-container">
-      <button className="count-btn">
+      <button
+        className="count-btn"
+        onClick={() => handleUpdateCount("decrement")}
+      >
         <MinusIcon className="count-btn-icon" />
       </button>
-      <button className="count-btn">
+      <button className="count-btn" onClick={() => handleUpdateCount()}>
         <PlusIcon className="count-btn-icon" />
       </button>
     </div>
